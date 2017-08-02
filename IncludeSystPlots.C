42a4021de9ffd1c7482db976af4bba5d42f1799f
brochero <javier.brochero.cifuentes@cern.ch>
2017-08-02 09:54:40 +0200

    adding additional histos and correct paths. WIP

--- IncludeSystPlots.C
+++ IncludeSystPlots.C
@@ -26,6 +26,9 @@
 #include <cmath>
 #include <set>
 
+std::vector<histos> loadhistogramsSYST (TString plots, TString namefile, TString namesyst="", TString namevar = "");
+
+
 void IncludeSystPlots(TString ttbarCat = "ttbb", TString plots="2btag", TString nSample = "ttbar"){
 
   /****************
@@ -34,12 +37,12 @@ void IncludeSystPlots(TString ttbarCat = "ttbb", TString plots="2btag", TString 
   
   enum FileSystematic:int{PileUp,
       JER, JES,
-      LES, LepSF,
+      //LES, LepSF,
       btagjes, btaglf, btaghf, btaghfsI, btaghfsII, btaglfsI, btaglfsII, btagcfI, btagcfII};
   
-  TString SystNam[14]= {"PileUp",
+  TString SystNam[12]= {"PileUp",
 			"JER", "JES",
-			"LES", "LepSF",
+			//"LES", "LepSF",
 			"btagjes", "btaglf", "btaghf", "btaghfsI", "btaghfsII", "btaglfsI", "btaglfsII", "btagcfI", "btagcfII"};
   
   enum FileVariation:int{Nom, Down, Up};
@@ -55,7 +58,7 @@ void IncludeSystPlots(TString ttbarCat = "ttbb", TString plots="2btag", TString 
   channel[2] = "lepjet"; 
 
   TString files   = dirnameIn + fl;
-  TString nttbarS = "ttbar_LepJetsPowhegPythiaTranche3";
+  TString nttbarS = "ttbar_LepJetsPowhegPythia";
   
   /****************
     ttbar Signal
@@ -64,34 +67,36 @@ void IncludeSystPlots(TString ttbarCat = "ttbb", TString plots="2btag", TString 
   std::vector<histos> Sample;
 
   TString SampleFullName;
-  if (nSample == "ttbar") SampleFullName = nttbarS + ttbarCat + "FullPhSp";
+  if (nSample == "ttbar") SampleFullName = nttbarS + ttbarCat;
   else SampleFullName = nSample;
 
-  Sample = loadhistograms(plots, files + "_" + SampleFullName);
+  Sample = loadhistogramsSYST(plots, files + "_" + SampleFullName);
 
-  // PileUp
-  std::vector<histos> SampleSys[14][3]; //[syst][var]
+  // All Systematic Uncertainties
+  std::vector<histos> SampleSys[12][3]; //[syst][var]
 
-  for(int isys = 0; isys<14; isys++){
+  for(int isys = 0; isys<12; isys++){
 
-    if (isys == JER)  SampleSys[JER][Nom] = loadhistograms(plots, files + "_" + SampleFullName + "_SYS_" + SystNam[isys] + "_" + VarNam[Nom]);      
+    // if (isys == JER)  SampleSys[JER][Nom] = loadhistograms(plots, files + "_" + SampleFullName + "_SYS_" + SystNam[isys] + "_" + VarNam[Nom]);      
     
     for(int ivar = 1; ivar<3; ivar++)
-      SampleSys[isys][ivar] = loadhistograms(plots, files + "_" + SampleFullName + "_SYS_" + SystNam[isys] + "_" + VarNam[ivar]);      
+      // SampleSys[isys][ivar] = loadhistogramsSYST(plots, files + "_" + SampleFullName + "_SYS_" + SystNam[isys] + VarNam[ivar]);      
+      SampleSys[isys][ivar] = loadhistogramsSYST(plots, files + "_" + SampleFullName, SystNam[isys], VarNam[ivar]);      
 
   }// for(isys)
 
-  
   for(unsigned int h = 0; h < Sample.size(); h++){
     for(unsigned int ch=0; ch<2; ch++){// Only mu+Jets and e+Jets  
       for(int ibin = 1; ibin <= Sample[h].hist[ch]->GetNbinsX()+1; ibin++){      
 	float central = Sample[h].hist[ch]->GetBinContent(ibin);
 
-	float SysVar[14][3];
-	for(int isys = 0; isys<14; isys++){
-	  if (isys == JER) SysVar[JER][Nom] = SampleSys[JER][Nom][h].hist[ch]->GetBinContent(ibin);
-	  for(int ivar = 1; ivar<3; ivar++)
-	    SysVar[isys][ivar] = SampleSys[isys][ivar][h].hist[ch]->GetBinContent(ibin);	 
+	float SysVar[12][3];
+	for(int isys = 0; isys<12; isys++){
+	  //if (isys == JER) SysVar[JER][Nom] = SampleSys[JER][Nom][h].hist[ch]->GetBinContent(ibin);
+	  for(int ivar = 1; ivar<3; ivar++){
+	    SysVar[isys][ivar] = SampleSys[isys][ivar][h].hist[ch]->GetBinContent(ibin);
+	  }
+
 	}
 
 
@@ -111,7 +116,8 @@ void IncludeSystPlots(TString ttbarCat = "ttbb", TString plots="2btag", TString 
 	float JESTot = 0.0;
 	if(central != 0.0) JESTot = max(fabs(central - SysVar[JES][Up])/central,  fabs(central - SysVar[JES][Down])/central);
 	float JERTot = 0.0;
-	if(SysVar[JER][Nom] != 0.0) JERTot = max(fabs(SysVar[JER][Nom] - SysVar[JER][Up])/SysVar[JER][Nom],  fabs(SysVar[JER][Nom] - SysVar[JER][Down])/SysVar[JER][Nom]);
+	// if(SysVar[JER][Nom] != 0.0) JERTot = max(fabs(SysVar[JER][Nom] - SysVar[JER][Up])/SysVar[JER][Nom],  fabs(SysVar[JER][Nom] - SysVar[JER][Down])/SysVar[JER][Nom]);
+	if(central != 0.0) JERTot = max(fabs(central - SysVar[JER][Up])/central,  fabs(central - SysVar[JER][Down])/central);
 	
 	
 	float Stat  = Sample[h].hist[ch]->GetBinError(ibin);
@@ -130,7 +136,16 @@ void IncludeSystPlots(TString ttbarCat = "ttbb", TString plots="2btag", TString 
 
 } //end Plots.C
 
-std::vector<histos> loadhistograms(TString plots, TString namefile){
+std::vector<histos> loadhistogramsSYST(TString plots, TString namefile, TString namesyst, TString namevar){
+
+  TString htailname = "";
+  TString hdirname = "central/";
+
+  if (namesyst != ""){
+    namefile = namefile + "_SYS_" + namesyst + namevar;      
+    htailname = "_" + namesyst + namevar;
+    hdirname =  namesyst + namevar + "/";
+  }
 
   TFile *file=NULL; // new TFile(namefile);
   file = TFile::Open(namefile + ".root");
@@ -171,13 +186,16 @@ std::vector<histos> loadhistograms(TString plots, TString namefile){
   histoname.push_back("hCSV_Jet-3");
   histoname.push_back("hCSV_Jet-4");
   histoname.push_back("hCSV_Jet-5");
-  histoname.push_back("hKinAdd1CSV");
-  histoname.push_back("hKinAdd2CSV");
+  histoname.push_back("hKinAdd1CSV_30");
+  histoname.push_back("hKinAdd2CSV_30");
+  histoname.push_back("hKinAdd12CSV_30");
   histoname.push_back("hInvMassjj");
   histoname.push_back("hmT");
+  histoname.push_back("hKinTagAddMass");
+  histoname.push_back("hKinTagAddDR");
 
   for(unsigned int h=0; h<histoname.size(); h++){
-    for(unsigned int ch=0; ch<2; ch++) histofile.hist[ch] = (TH1D*)file->Get(plots + "/" + channel[ch] + "/" +  histoname[h] + "_" + channel[ch] + "_" + plots);    
+    for(unsigned int ch=0; ch<2; ch++) histofile.hist[ch] = (TH1D*)file->Get(hdirname + plots + "/" + channel[ch] + "/" +  histoname[h] + "_" + channel[ch] + "_" + plots + htailname);
 
     sample.push_back(histofile);
     
@@ -200,19 +218,18 @@ void overwritehistograms(std::vector<histos> newhistos, TString plots, TString n
   TString namecut    [7]   = {"lepton","6Jets","2btag","3btag","4Jets","4Jets2btag","Only2btag"};
 
   for (int ic=0;ic<7;ic++) {
-    file->mkdir(namecut[ic]);
-    file->mkdir(namecut[ic] + "/mujets");
-    file->mkdir(namecut[ic] + "/ejets");
+    file->mkdir("central/" + namecut[ic] + "/mujets");
+    file->mkdir("central/" + namecut[ic] + "/ejets");
   }
 
   for(unsigned int h = 0; h < newhistos.size(); h++){
     for(unsigned int ch=0; ch<2; ch++){// Only mu+Jets and e+Jets  
-      file->cd   (plots + "/" + namech[ch]);
+      file->cd   ("central/" + plots + "/" + namech[ch]);
       //newhistos[h].hist[ch]->Write("",TObject::kOverwrite);
       newhistos[h].hist[ch]->Write();
     }
   }
   
-  cout << "All the histograms from " << namefile << ".root have been overwrote successfully!!!"  << endl; 
+  cout << "All the histograms from " << namefile << ".root have been copied with the corresponding Syst Unc successfully!!!"  << endl; 
 }
 

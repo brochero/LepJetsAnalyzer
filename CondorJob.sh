d05c83c681abe289821e315a00c67bf82c72c7da
Javier Brochero <javier.brochero.cifuentes@cern.ch>
2018-06-28 22:28:11 +0900

    Correct path

--- CondorJob.sh
+++ CondorJob.sh
@@ -11,7 +11,7 @@ output=$2
 cd /cms/scratch/brochero/CATTuples_2017/CMSSW_8_0_20/src/
 scram build ProjectRename
 eval `scram runtime -sh`
-cd /cms/home/brochero/brochero_WorkArea/LepJetsAnalyzer/
+cd /cms/scratch/brochero/LepJetsAnalyzer/
 echo "To run with CONDOR eos should be used!"
 echo ./TreeReader.run -eos -i $input -o $output
 ./TreeReader.run -eos -i $input -o $output

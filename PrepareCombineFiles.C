a084023237c2f347deca337225894c157cf8e130
brochero <javier.brochero.cifuentes@cern.ch>
2018-09-26 10:59:35 +0200

    Adding Stat unc and variations og ttbj/ttbb

--- PrepareCombineFiles.C
+++ PrepareCombineFiles.C
@@ -33,7 +33,6 @@ void PrepareCombineFiles(TString FileVersion = "PreApp-v0", TString FileProd = "
   SysName.push_back("JER");
   SysName.push_back("btag_LF");SysName.push_back("btag_HF");SysName.push_back("btag_HFStats1");SysName.push_back("btag_HFStats2");SysName.push_back("btag_LFStats1");SysName.push_back("btag_LFStats2");SysName.push_back("btag_cErr1");SysName.push_back("btag_cErr2");
   SysName.push_back("JESAbsoluteStat");SysName.push_back("JESAbsoluteScale");SysName.push_back("JESAbsoluteMPFBias");SysName.push_back("JESFragmentation");SysName.push_back("JESSinglePionECAL"); SysName.push_back("JESSinglePionHCAL");SysName.push_back("JESFlavorQCD");SysName.push_back("JESTimePtEta");SysName.push_back("JESRelativeJEREC1"); SysName.push_back("JESRelativeJEREC2"); SysName.push_back("JESRelativeJERHF");SysName.push_back("JESRelativePtBB"); SysName.push_back("JESRelativePtEC1"); SysName.push_back("JESRelativePtEC2");SysName.push_back("JESRelativePtHF");SysName.push_back("JESRelativeBal");SysName.push_back("JESRelativeFSR");SysName.push_back("JESRelativeStatFSR"); SysName.push_back("JESRelativeStatEC"); SysName.push_back("JESRelativeStatHF");SysName.push_back("JESPileUpDataMC");SysName.push_back("JESPileUpPtRef");SysName.push_back("JESPileUpPtBB");SysName.push_back("JESPileUpPtEC1");SysName.push_back("JESPileUpPtHF");
-    
   
   
   std::vector<TString> TTbarNam, BkgNam, STNam, VVNam;
@@ -117,9 +116,35 @@ void MergeSystematics(TString Sample, std::vector<TString> SysName){
     target->cd   ("central/2btag/" + channel[ch]);
     histofile->Write();
 
+    // Statistical Uncertainties
     if(Sample.Contains("ttbar_LepJetsPowhegPythiatt") ||
        Sample.Contains("ttbar_PowhegPythiaBkgtt") ||
        Sample.Contains("SingleTop")){
+      
+      TH1D *histofileStatUp   = (TH1D*)histofile->Clone();
+      TH1D *histofileStatDown = (TH1D*)histofile->Clone();
+      
+      TString ttName = Sample(Sample.Length()-4,Sample.Length());// 4 is the number of characters in tt**
+      if (ttName == "iatt" || ttName == "kgtt") ttName = "tt"; // for tt
+      if (ttName == "eTop") ttName = "st"; // for Single Top
+      
+      GetStatVar(histofile, histofileStatDown, histofileStatUp, ttName);
+      
+      target->cd();
+      target->mkdir(ttName + "StatUp/2btag/" + channel[ch]);
+      target->mkdir(ttName + "StatDown/2btag/" + channel[ch]);
+
+      target->cd(ttName + "StatUp/2btag/" + channel[ch]);
+      histofileStatUp->Write();
+      target->cd(ttName + "StatDown/2btag/" + channel[ch]);
+      histofileStatDown->Write();
+
+    }
+
+    // ttbb/ttbj Ratio
+    // Statistical Uncertainties
+    if(Sample.Contains("ttbar_LepJetsPowhegPythiattbb") ||
+       Sample.Contains("ttbar_LepJetsPowhegPythiattbj")){
       
       TH1D *histofileStatUp   = (TH1D*)histofile->Clone();
       TH1D *histofileStatDown = (TH1D*)histofile->Clone();
@@ -138,6 +163,7 @@ void MergeSystematics(TString Sample, std::vector<TString> SysName){
       histofileStatUp->Write();
       target->cd(ttName + "StatDown/2btag/" + channel[ch]);
       histofileStatDown->Write();
+
     }
     
     
@@ -150,14 +176,14 @@ void MergeSystematics(TString Sample, std::vector<TString> SysName){
       TString SystVarNam = SysName.at(isys)+SysVar[ivar];
       TString SystVarNamFile = SysName.at(isys)+SysVar[ivar];
       
-      if(Sample.Contains("Bkgtt")   &&
-	 (SysName.at(isys) == "ISR" ||
-	  SysName.at(isys) == "FSR" ||
-	  SysName.at(isys) == "UE"  ||
-	  SysName.at(isys) == "ScaleRdF")) SystVarNamFile = "AllTheory";  
-      else if (SysName.at(isys) == "ISR" ||
-	       SysName.at(isys) == "FSR" ||
-	       SysName.at(isys) == "UE") SystVarNamFile = "Theory";  
+      // if(Sample.Contains("Bkgtt")   &&
+      // 	 (SysName.at(isys) == "ISR" ||
+      // 	  SysName.at(isys) == "FSR" ||
+      // 	  SysName.at(isys) == "UE"  ||
+      // 	  SysName.at(isys) == "ScaleRdF")) SystVarNamFile = "AllTheory";  
+      // else if (SysName.at(isys) == "ISR" ||
+      // 	       SysName.at(isys) == "FSR" ||
+      // 	       SysName.at(isys) == "UE") SystVarNamFile = "Theory";  
       
       TFile *FilesSam = TFile::Open(Sample+"_SYS_"+SystVarNamFile+".root");
       cout << "Open file " << Sample+"_SYS_"+SystVarNamFile+".root" << endl;
